c5d614b23aa4fecf55ac95019044e946a7c6a290
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-16 23:59:15 +0200

    macartequituelamort

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./components/Card"
+import "./style.scss";
+
+function App() {
+  return (
+   <Card/>
+  );
+}
+
+export default App;
+
---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+
+function Card() {
+
+    return (
+        <>
+            <body>
+                <article>
+                    <section className="music">
+                    <img src="https://image.ausha.co/0pSl3PhgvJn4OJMRb95ZpZ9RfvGBozmpgF7XnFbi_1400x1400.jpeg?t=1707313783" alt="pochette album" />
+                    <div>
+                        <h1>
+                            M83
+                        </h1>
+                        <h2>
+                            <p>Midnight City</p>
+                            <p>(2011)</p>
+                        </h2>
+                    </div>
+                </section>
+                <section className="star">
+                    <h2> Rate this album </h2>
+                    <button> ⭐ ⭐ ⭐ ⭐ ⭐ </button>
+                </section>
+                </article>
+            </body>
+        </>
+    )
+}
+
+
+
+
+
+export default Card;
